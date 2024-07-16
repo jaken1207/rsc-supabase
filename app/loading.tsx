5da7bb499ab27707f7ce298c30fd70c3ea8eea1f
DE-nagata <nagata@defide-ix.com>
2024-07-16 15:11:44 +0900

    [update]loadingの追加

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Spinner from './components/spinner'
+
+const Loading = () => {
+  return (
+    <div>
+      <Spinner />
+    </div>
+  )
+}
+
+export default Loading
