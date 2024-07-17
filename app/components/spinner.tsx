4c46b0dfa7a1ca41bbeeec4ae38b449421343e55
DE-nagata <nagata@defide-ix.com>
2024-07-18 08:56:51 +0900

    [update]daisyUIのloading導入

--- app/components/spinner.tsx
+++ app/components/spinner.tsx
@@ -3,9 +3,10 @@ import React from 'react'
 const Spinner = ({ color = 'border-blue-500' }: { color?: string }) => {
   return (
     <div className="my-16 flex justify-center">
-      <div
-        className={`h-10 w-10 animate-spin rounded-full border-4 ${color} border-t-transparent`}
-      />
+      {/* <div
+      className={`h-10 w-10 animate-spin rounded-full border-4 ${color} border-t-transparent`}
+      /> */}
+      <span className={`loading loading-infinity loading-lg ${color}`}></span>
     </div>
   )
 }
